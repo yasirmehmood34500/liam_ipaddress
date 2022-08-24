fcc8c432e0ae6ed8a1538142cd87aa493431543c
Yasir Memood <yasirmehmood34500@gmail.com>
2022-08-24 20:11:21 +0500

    .bat file exm

--- delete.php
+++ delete.php
@@ -1,7 +1,4 @@
 <?php include "inc/programming.php";
 $dele=$conn->query("DELETE FROM data_list WHERE id > 0");
-if ($dele) {
-	rmdir("upload");
-}
 echo "<script>window.location='view.php'</script>";
  ?>
